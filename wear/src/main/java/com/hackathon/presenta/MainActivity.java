f24f71967a1b00193484b1dd66f97e3a75d67521
soima <sergiu.soima@gmail.com>
2014-11-08 18:32:05 +0100

    ready programmiert

--- wear/src/main/java/com/hackathon/presenta/MainActivity.java
+++ wear/src/main/java/com/hackathon/presenta/MainActivity.java
@@ -1,24 +1,43 @@
 package com.hackathon.presenta;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import static com.hackathon.presenta.R.id.*;
 
 public class MainActivity extends Activity {
 
     private TextView mTextView;
+    private Button ready;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        final WatchViewStub stub = (WatchViewStub) findViewById(watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
+                mTextView = (TextView) stub.findViewById(text);
+
+                ready = (Button) stub.findViewById(readybutton);
+                ready.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent k = new Intent(MainActivity.this, step_1.class);
+                        startActivity(k);
+                    }
+                });
+
+
             }
         });
+
     }
+
 }
